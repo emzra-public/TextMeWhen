a6bd826c8023e8d7dbf78625504622ffd7649c06
Emaline G <mgayhart0@ivc.edu>
2022-10-30 20:22:00 -0700

    changed minor aesthetics

--- src/components/EventForm.tsx
+++ src/components/EventForm.tsx
@@ -25,7 +25,7 @@ export default function EventForm() {
       setDate("");
       setTime("");
       setSuccessMsg(
-        "Successfully pushed to DB! We will soon have functionality to create your own opt-in page."
+        "We will soon have functionality to create your own opt-in page."
       );
     } else if (name.length === 0) {
       setNameError("Please enter a name for your event");
@@ -37,68 +37,74 @@ export default function EventForm() {
   };
   return (
     <>
-      <div>
-        <label className="block text-sm font-medium text-gray-700">
-          Event Name
-        </label>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        ></input>
-        {nameError && <p className="mt-2 text-sm text-red-600">{nameError}</p>}
-      </div>
-
-      <div className="mt-10 mb-6 flex flex-col items-center justify-center gap-y-6 gap-x-4">
-        <div className="relative">
-          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-            <svg
-              aria-hidden="true"
-              className="h-5 w-5 text-gray-500 dark:text-gray-400"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </div>
-          <input
-            type="date"
-            className="text-white-900 block w-full rounded-lg border border-orange-300 bg-orange-50 p-2.5 pl-10 sm:text-sm"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-          ></input>
-          {dateError && (
-            <p className="mt-2 text-sm text-red-600">{dateError}</p>
-          )}
-        </div>
+      <div className="flex flex-col items-center justify-center">
         <div>
           <label className="block text-sm font-medium text-gray-700">
-            Event Time:
+            Event Name
           </label>
           <input
-            type="time"
-            value={time}
-            onChange={(e) => setTime(e.target.value)}
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             required
           ></input>
-          {timeError && (
-            <p className="mt-2 text-sm text-red-600">{timeError}</p>
+          {nameError && (
+            <p className="mt-2 text-sm text-red-600">{nameError}</p>
           )}
         </div>
+
+        <div className="mt-10 mb-6 flex flex-col items-center justify-center gap-y-6 gap-x-4">
+          <div className="relative">
+            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+              <svg
+                aria-hidden="true"
+                className="h-5 w-5 text-gray-500 dark:text-gray-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </div>
+            <input
+              type="date"
+              className="text-white-900 block w-full rounded-lg border border-orange-300 bg-orange-50 p-2.5 pl-10 sm:text-sm"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            ></input>
+            {dateError && (
+              <p className="mt-2 text-sm text-red-600">{dateError}</p>
+            )}
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Event Time:
+            </label>
+            <input
+              type="time"
+              value={time}
+              onChange={(e) => setTime(e.target.value)}
+              required
+            ></input>
+            {timeError && (
+              <p className="mt-2 text-sm text-red-600">{timeError}</p>
+            )}
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="inline-flex justify-center rounded-md border border-transparent bg-orange-300 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-orange-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              onClick={handleEventSubmit}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
         <div>
-          <button
-            type="submit"
-            className="inline-flex justify-center rounded-md border border-transparent bg-orange-300 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-orange-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            onClick={handleEventSubmit}
-          >
-            Submit
-          </button>
           {successMsg && (
             <p className="mt-2 text-sm text-red-600">{successMsg}</p>
           )}
