c16ab37a8cdad0ba8c989cb1c9b64939a99503b5
emalineg <mgayhart0@ivc.edu>
2022-12-06 10:13:02 -0800

    added grid columns

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,22 +26,27 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
-        <h1 className="text-6xl font-extrabold leading-normal text-gray-700 md:text-[5rem]">
-          TextMeWhen<span className="text-orange-300">...</span>
-        </h1>
-        <p className="mb-2 text-xl text-gray-700 md:text-2xl">
-          Scheduled Text Delivery and Opt-In Page Hosting
-        </p>
-        <p className="text-m md:text-l mb-5 italic text-gray-700">
-          Version 1.0: Supports events min. 1hr/max. 7 days in future.
-        </p>
+      <div className="grid-template-columns: 1fr 3fr; grid">
+        <div className="grid-column: 1;">...</div>
+        <div className="grid-column: 1;">...</div>
+        <div className="grid-column: 1;">...</div>
+        <main className="grid-column: 2; container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
+          <h1 className="text-6xl font-extrabold leading-normal text-gray-700 md:text-[5rem]">
+            TextMeWhen<span className="text-orange-300">...</span>
+          </h1>
+          <p className="mb-2 text-xl text-gray-700 md:text-2xl">
+            Scheduled Text Delivery and Opt-In Page Hosting
+          </p>
+          <p className="text-m md:text-l mb-5 italic text-gray-700">
+            Version 1.0: Supports events min. 1hr/max. 7 days in future.
+          </p>
 
-        <div>
-          <EventForm />
-        </div>
-        <p className="text-gray-700"></p>
-      </main>
+          <div>
+            <EventForm />
+          </div>
+          <p className="text-gray-700"></p>
+        </main>
+      </div>
     </>
   );
 };
