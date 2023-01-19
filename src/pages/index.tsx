a30b466b1f041731cd3c59bb8bb9a1c07fd98b47
EmalineG <mgayhart0@ivc.edu>
2023-01-18 20:47:10 -0800

    mobile content adjustment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
 
       <div className="grid-template-columns-1fr3fr grid-auto-flow-column grid gap-2">
         <div className="sm: float-left"></div>
-        <main className="container mx-auto flex max-h-screen flex-col items-center justify-center p-10">
+        <main className="container mx-auto flex flex-col items-center justify-center p-10">
           <h1 className="text-6xl font-extrabold leading-normal text-gray-700 sm:text-[5rem]">
             TextMeWhen<span className="text-orange-300">...</span>
           </h1>
