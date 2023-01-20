88ac86529dc9a3e3d6eb5d9fcc43d19452095164
EmalineG <mgayhart0@ivc.edu>
2023-01-19 21:39:12 -0800

    centered text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,10 +21,10 @@ const Home: NextPage = () => {
           <h1 className="text-6xl font-extrabold leading-normal text-gray-700 sm:text-[5rem]">
             TextMeWhen<span className="text-orange-300">...</span>
           </h1>
-          <p className="mb-2 text-xl text-gray-700 sm:text-2xl">
+          <p className="mb-2 text-xl text-gray-700 text-center sm:text-xl">
             Scheduled Text Delivery and Opt-In Page Hosting
           </p>
-          <p className="text-m sm:text-l mb-5 italic text-gray-700">
+          <p className="text-m text-center sm:text-l mb-5 italic text-gray-700">
             Version 1.0: Supports events min. 1hr & max. 7 days in future.
           </p>
 
