2b19fa607e2dd1cf30fac0a1c1af4b4ddfd9ccec
EmalineG <mgayhart0@ivc.edu>
2023-01-19 21:34:47 -0800

    added slight padding to l & r

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
 
       <div className="grid-template-columns-1fr grid">
     
-        <main className="container mx-auto flex flex-col items-center justify-center pt-10">
+        <main className="container mx-auto flex flex-col items-center justify-center pt-10 pl-2 pr-2">
           <h1 className="text-6xl font-extrabold leading-normal text-gray-700 sm:text-[5rem]">
             TextMeWhen<span className="text-orange-300">...</span>
           </h1>
