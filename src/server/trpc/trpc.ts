0ce8f57f459a11d368ff514727f2d1907d612397
emalinegayhart <emgayhart@gmail.com>
2026-03-20 08:21:22 -0700

    chore: remove auth

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -1,4 +1,4 @@
-import { initTRPC, TRPCError } from "@trpc/server";
+import { initTRPC } from "@trpc/server";
 import superjson from "superjson";
 import { type Context } from "./context";
 
@@ -10,29 +10,4 @@ const t = initTRPC.context<Context>().create({
 });
 
 export const router = t.router;
-
-/**
- * Unprotected procedure
- **/
 export const publicProcedure = t.procedure;
-
-/**
- * Reusable middleware to ensure
- * users are logged in
- */
-const isAuthed = t.middleware(({ ctx, next }) => {
-  if (!ctx.session || !ctx.session.user) {
-    throw new TRPCError({ code: "UNAUTHORIZED" });
-  }
-  return next({
-    ctx: {
-      // infers the `session` as non-nullable
-      session: { ...ctx.session, user: ctx.session.user },
-    },
-  });
-});
-
-/**
- * Protected procedure
- **/
-export const protectedProcedure = t.procedure.use(isAuthed);
